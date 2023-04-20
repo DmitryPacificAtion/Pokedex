dbbc758c1def47d74eb5a47ca8c09ba4eabac28e
DmitryPacificAtion <bruslik.dmitry@gmail.com>
2023-04-20 11:04:41 +0300

    Test loader

---
+++ src/components/__tests__/Pokemon.test.js
@@ -0,0 +1,33 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import nock from 'nock';
+
+// import { renderHook, act } from '@testing-library/react-hooks'
+// import { useParams } from "react-router-dom"
+
+import Pokemon from '../pages/Pokemon';
+// import Result from '../sections/Result';
+// import Message from "../sections/Message";
+
+jest.mock('../../components/pages/Loader', () => ({
+  default: props => <span>Loader</span>,
+  __esModule: true
+}));
+
+// or
+// jest.mock('../../components/pages/Loader', () => props => <span>Loader</span>);
+
+nock(`https://pokeapi.co/api/v2`).get('/pokemon/1').reply(() => new Promise(() => {}))
+
+describe('Test Pokemon page', () => {
+  test('Loader has been shown', () => {
+    render(<Pokemon />)
+
+    // const useFetch = jest.fn();
+    // const useFetchSpy = jest.spyOn(window, useFetch);
+    // useFetchSpy.mockImplementation(() => []);
+    // render(<Pokemon />)
+    expect(screen.getByText('Loader')).toBeInTheDocument()
+  });
+});
+
